f8bfeecc5bef949de96df8a807d8cd154dd36dfd
Kyle Whitaker <kwhitaker@level20.com>
2020-01-20 10:06:29 -0500

    a simple query

--- src/index.js
+++ src/index.js
@@ -1,22 +1,41 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-// 1
 const typeDefs = `
 type Query {
   info: String!
+  feed: [Link!]!
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
 }
 `;
 
-// 2
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL"
+  }
+];
+
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links
+  },
+  Link: {
+    id: parent => parent.id, // since trivial, actually not necessary
+    url: parent => parent.url,
+    description: parent => parent.description
   }
 };
 
-// 3
 const server = new GraphQLServer({
   typeDefs,
   resolvers
 });
+
 server.start(() => console.log(`Server is running on http://localhost:4000`));
